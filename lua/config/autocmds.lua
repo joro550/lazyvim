15d39a612ae0ab92b42a473e661d5c56f7c1223b
root <root@Yogi>
2024-03-15 18:20:49 +0000

    Adding lsp

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.filetype.add({ extension = { templ = "templ" } })
+
+vim.lsp.start({
+  name = "dbt-lsp",
+  cmd = { "/root/dev/go/dbt-language-server/out/dbt-lsp" },
+  root_dir = vim.fs.dirname(vim.fs.find({ "dbt_project.yml" }, { upward = true })[1]),
+})
